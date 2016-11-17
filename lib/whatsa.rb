e6bb11f808ea0d92e8e982b445b5c055b52fe0ca
Keegan Leitz <kjleitz@gmail.com>
2016-11-17 21:04:11 +0000

    set up basic env with some requires

--- lib/whatsa.rb
+++ lib/whatsa.rb
@@ -1,5 +1,10 @@
-require "whatsa/version"
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+require_relative 'whatsa/version'
+require_relative 'whatsa/cli'
+# require_relative 'whatsa/articler'
 
 module Whatsa
-  # Your code goes here...
 end
