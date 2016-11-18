e27dbeceb27f4904ad7515e1f8cdf8122a40ab15
Keegan Leitz <kjleitz@gmail.com>
2016-11-18 22:09:19 +0000

    explanatory comments

--- lib/whatsa/scraper.rb
+++ lib/whatsa/scraper.rb
@@ -1,12 +1,15 @@
 class Whatsa::Scraper
-  attr_reader :query, :page
 
   WIKISEARCH = 'https://en.wikipedia.org/w/index.php?search='
+
+  attr_reader :query, :page
 
   def initialize(term)
     # only keep word chars, turn everything between each 'word' to a single '+'
     # and remove '+'s at the beginning and end if they're there
     @query = term.gsub(/\W+/, '+').gsub(/(\A\+|\+\z)/, '')
+
+    # store the page in an instance variable so we don't keep polling the site
     @page = Nokogiri::HTML(open(WIKISEARCH + self.query))
   end
 
@@ -27,7 +30,7 @@ class Whatsa::Scraper
   end
 
   def make_article
-    
+
   end
 
   def make_disambig
