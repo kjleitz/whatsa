add58304e62d00364f36b91575e7e1cdaf8c8500
Keegan Leitz <kjleitz@gmail.com>
2016-11-18 22:08:07 +0000

    Articles init w/ a nokogiri doc and have a title, can get intro paragraphs

--- lib/whatsa/article.rb
+++ lib/whatsa/article.rb
@@ -1,3 +1,28 @@
 class Whatsa::Article
+  attr_reader :contents, :title
 
+  def initialize(noko_doc)
+    @page = noko_doc
+    @title = noko_doc.css('h1').text
+    # @contents = make_contents
+  end
+
+  def make_contents
+    
+  end
+
+  def intro_pars
+    content = page.css('#mw-content-text').children
+    breakpoint = content.to_a.index { |element| element.name == 'h2' }
+    content[0...breakpoint].css('p').map { |par| par.text }
+  end
+
+  private
+
+  # making the getter private; directly looking at the nokogiri document is a job
+  # for the Scraper class... the Article class is for handling the article as an
+  # abstract-ish concept
+  def page
+    @page
+  end
 end
