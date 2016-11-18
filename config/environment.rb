baf2cd669a213d0585132f1c941b3ec7d9d8a17a
Keegan Leitz <kjleitz@gmail.com>
2016-11-18 03:04:02 +0000

    rearranged env requires

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+require_relative 'whatsa/version'
+require_relative 'whatsa/cli'
+require_relative 'whatsa/article'
+require_relative 'whatsa/section'
--- lib/whatsa.rb
+++ lib/whatsa.rb
@@ -1,10 +1,4 @@
-require 'open-uri'
-require 'nokogiri'
-require 'pry'
-
-require_relative 'whatsa/version'
-require_relative 'whatsa/cli'
-# require_relative 'whatsa/articler'
+require_relative '../config/environment'
 
 module Whatsa
 end
