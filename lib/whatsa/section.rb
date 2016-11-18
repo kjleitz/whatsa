6c1adfa3f67049fe258e0e850262f0921bdaf6bb
Keegan Leitz <kjleitz@gmail.com>
2016-11-18 22:10:19 +0000

    sections init w/ title and array of paragraphs, can give their first paragraph as a summary

--- lib/whatsa/section.rb
+++ lib/whatsa/section.rb
@@ -1,3 +1,17 @@
 class Whatsa::Section
+  attr_accessor :title, :paragraphs
+
+  def initialize(title, paragraphs)
+    @title = title
+    @paragraphs = paragraphs
+  end
+
+  def summary
+    self.paragraphs.first
+  end
+
+  def full_text
+    
+  end
 
 end
