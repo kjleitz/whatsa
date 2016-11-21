ee6b768a66eebde22652844bf2aa6171adf826a1
Keegan Leitz <kjleitz@gmail.com>
2016-11-21 21:21:44 +0000

    commented stuff

--- lib/whatsa.rb
+++ lib/whatsa.rb
@@ -3,7 +3,7 @@ require_relative '../config/environment'
 module Whatsa
 end
 
-feces = Whatsa::Article.new(Whatsa::Scraper.new('feces').page)
-poop = Whatsa::Disambig.new(Whatsa::Scraper.new('poop').page)
-
-binding.pry
+# feces = Whatsa::Article.new(Whatsa::Scraper.new('feces').page)
+# poop = Whatsa::Disambig.new(Whatsa::Scraper.new('poop').page)
+#
+# binding.pry
