cfc18010534805d0f0ac501aa40ae089701ec8f3
Keegan Leitz <kjleitz@gmail.com>
2016-11-21 19:19:22 +0000

    made disambig class, inits w/ title + items + description hash

--- lib/whatsa/disambig.rb
+++ lib/whatsa/disambig.rb
@@ -1,3 +1,23 @@
 class Whatsa::Disambig
+  attr_accessor :descriptions
+  attr_reader :title, :items
+
+  def initialize(noko_doc)
+    @title = noko_doc.css('h1').text
+    @items = noko_doc.css('#mw-content-text li')
+    @descriptions = make_descriptions
+  end
+
+  private
+
+  # make a hash with links and their descriptions
+  def make_descriptions
+    self.items.inject({}) do |memo, item|
+      unless item.css('a').empty?
+        memo[item.css('a').first.text] = item.text.split("\n").first
+      end
+      memo
+    end
+  end
 
 end
