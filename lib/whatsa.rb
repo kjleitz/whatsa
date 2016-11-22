5ef101d92371738a1f76d213ff9d53552e61ed0d
Keegan Leitz <kjleitz@gmail.com>
2016-11-22 17:02:12 +0000

    removed unnecessary comments

--- lib/whatsa.rb
+++ lib/whatsa.rb
@@ -2,8 +2,3 @@ require_relative '../config/environment'
 
 module Whatsa
 end
-
-# feces = Whatsa::Article.new(Whatsa::Scraper.new('feces').page)
-# poop = Whatsa::Disambig.new(Whatsa::Scraper.new('poop').page)
-#
-# binding.pry
