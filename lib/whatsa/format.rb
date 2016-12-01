abb796f5cb92ea7dee70519c53ed234fc3fd5e7c
Keegan Leitz <kjleitz@gmail.com>
2016-12-01 16:15:17 +0000

    added #heading_to_title which strips and removes [edit] markers

--- lib/whatsa/format.rb
+++ lib/whatsa/format.rb
@@ -5,5 +5,9 @@ module Whatsa
       string.gsub(/[^A-z0-9\(\)]+/, '+').gsub(/(\A\+|\+\z)/, '')
     end
 
+    def heading_to_title(string)
+      string.gsub('[edit]', '').strip
+    end
+
   end
 end
