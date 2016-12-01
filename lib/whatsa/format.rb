52b7f78b825258feded31ae072b61b8aa7ab6383
Keegan Leitz <kjleitz@gmail.com>
2016-12-01 15:31:37 +0000

    added #url_friendly to URL-ize input

--- lib/whatsa/format.rb
+++ lib/whatsa/format.rb
@@ -1,3 +1,10 @@
-class Whatsa::Format
-  
+module Whatsa
+  module Format
+
+    def url_friendly(string)
+      string.gsub(/[^A-z0-9\(\)]+/, '+').gsub(/(\A\+|\+\z)/, '')
+    end
+
+    
+  end
 end
