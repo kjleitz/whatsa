d2be40e787ba75471d8c555755bfcd44b094d458
Keegan Leitz <kjleitz@gmail.com>
2016-12-01 16:36:16 +0000

    added #word_wrap

--- lib/whatsa/format.rb
+++ lib/whatsa/format.rb
@@ -17,5 +17,21 @@ module Whatsa
       string.index("\n") ? string.gsub(/^/, "- ") : string
     end
 
+    # setting an indent will indent the lines AFTER the first line of a paragraph
+    def word_wrap(text, indent=0)
+      count = 0
+      words = text.split(/ /)
+      words.each_with_index do |word, index|
+        count += word.length + 1
+        if count > 80
+          words.insert(index, "\n#{' ' * indent}")
+          count = indent
+        elsif word.index("\n")
+          count = word.length
+        end
+      end
+      words.join(" ").gsub(/^ /, "")
+    end
+
   end
 end
